2cd999cfa6b1b5b70520cb40f9968654f68005a5
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-08 01:12:33 -0400

    add more stuff

--- ideal-pancake/v2/soil_sensor.py
+++ ideal-pancake/v2/soil_sensor.py
@@ -5,10 +5,6 @@ class SoilSensor(ABC):
 
     def __init__(self):
         super(SoilSensor, self).__init__()
-    
-    @abstractmethod
-    def get_moisture_percentage(self):
-        pass
         
     @abstractmethod
     def get_soil_state(self)-> Soil:
