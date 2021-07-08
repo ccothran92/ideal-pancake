4bf3bb39510a75aeefef084a0112dcf31f0738d9
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-08 01:25:55 -0400

    add more stuff

--- ideal-pancake/v2/kasa_outlet.py
+++ ideal-pancake/v2/kasa_outlet.py
@@ -18,7 +18,7 @@ class KasaOutlet(object):
 
     async def _state_info(self):
         await self.outlet.update()
-        await print(self.outlet.state_information)
+        print(await self.outlet.state_information)
 
     def _turn_on(self):
         asyncio.run(self.outlet.turn_on())
