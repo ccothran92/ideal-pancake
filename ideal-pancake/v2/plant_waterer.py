1e20613cc42415b961b233ff4bbf1985a21c8190
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-08 01:12:54 -0400

    add more stuff

--- ideal-pancake/v2/plant_waterer.py
+++ ideal-pancake/v2/plant_waterer.py
@@ -5,7 +5,7 @@ from soil_sensor import SoilSensor
 class PlantWaterer(object):
     def __init__(self, plant: Plant, soilSensor: SoilSensor): #TODO: Is this an unnecessary layer of abstraction? Probably. But I want to decouple specific types of sensors from
         self.plant = plant
-        self.soilSensor = sensor
+        self.soilSensor = soilSensor
 
     def needs_watering(self):
         return self.plant.desired_moisture_percentage < self.soilSensor.get_soil_state().get_moisture_percentage()
