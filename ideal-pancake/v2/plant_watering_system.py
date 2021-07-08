79d92fa6cf3d6ac478a5f1b667b9a3e5aa109c62
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-08 00:49:49 -0400

    add more stuff

--- ideal-pancake/v2/plant_watering_system.py
+++ ideal-pancake/v2/plant_watering_system.py
@@ -2,6 +2,7 @@ from soil_sensor import SoilSensor
 from plant import Plant
 from soil import Soil
 from watering_device import WateringDevice
+from plant_waterer import PlantWaterer
 from kasa_outlet import KasaOutlet
 from binary_soil_sensor import BinarySoilSensor
 import time
@@ -12,11 +13,10 @@ class PlantWateringSystem(object):
     def __init__(plant: Plant, watering_device: WateringDevice, sensor: SoilSensor):
         self.gpioPin = gpioPin
         self.watering_device = watering_device
-        self.sensor = sensor
-
+        self.plant_waterer = PlantWaterer(plant, sensor)
     def start(self):
         while True:
-        if self.sensor.needs_watering():
+        if self.plant_waterer.needs_watering():
             self.watering_device.water_my_plants()
         else: 
             self.watering_device.dont_water_my_plants()
