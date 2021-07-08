41fc7a6fa729eaf3980f709566909a7810f26a9f
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-08 00:52:57 -0400

    add more stuff

--- ideal-pancake/v2/my_thing.py
+++ ideal-pancake/v2/my_thing.py
@@ -1,6 +1,7 @@
 from plant_watering_system import PlantWateringSystem
 from kasa_outlet import KasaOutlet
 from plant import Plant
+from binary_soil_sensor import BinarySoilSensor
 
 waterer = KasaOutlet("192.168.1.2")
 myPlant = Plant(80, 1)
