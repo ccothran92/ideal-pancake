e1390a8a5dbc476efd548599bb2e8c78707a9e18
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-08 18:37:38 -0400

    add more stuff

--- ideal-pancake/v2/plant.py
+++ ideal-pancake/v2/plant.py
@@ -2,3 +2,4 @@ class Plant(object):
     def __init__(self, desired_moisture_percentage: int, dry_period_in_hours: int):
         self.desired_moisture_percentage = desired_moisture_percentage
         self.dry_period_in_hours = dry_period_in_hours
+        
---
+++ ideal-pancake/v2/turn_off_kasa.py
@@ -0,0 +1,4 @@
+from kasa_outlet import KasaOutlet
+
+kasa = KasaOutlet("192.168.1.2")
+kasa.dont_water_my_plants()
