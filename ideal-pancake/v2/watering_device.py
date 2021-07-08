1b808be3c2b486f2aaf4d936c515c6e7d0218c2d
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-07 22:53:35 -0400

    add more stuff

--- ideal-pancake/v2/watering_device.py
+++ ideal-pancake/v2/watering_device.py
@@ -5,10 +5,10 @@ class WateringDevice(object):
     def __init__(self, device: KasaOutlet): #should be extendable to not just be kasa. i.e. gpio out
         self.device = device
 
-    def water_my_plants():
+    def water_my_plants(self):
         print("Attempting to water plants")
         self.device.turn_on()
     
-    def dont_water_my_plants():
+    def dont_water_my_plants(self):
         print("Stopping the watering of plants")
         self.device.turn_off()
