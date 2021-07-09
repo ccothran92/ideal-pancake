ed999e2d39a44cf2e4bf2d80fb1dc71bbb3850ee
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-09 15:23:24 -0400

    add more stuff

--- ideal-pancake/v2/plant_watering_system.py
+++ ideal-pancake/v2/plant_watering_system.py
@@ -5,7 +5,8 @@ from watering_device import WateringDevice
 from plant_waterer import PlantWaterer
 import time
 
-SLEEP_TIME = 3
+SLEEP_TIME_REST = 3
+SLEEP_TIME_WHEN_WATERING = 1
 
 class PlantWateringSystem(object):
     def __init__(self, plant: Plant, watering_device: WateringDevice, sensor: SoilSensor):
@@ -16,6 +17,7 @@ class PlantWateringSystem(object):
         while True:
             if self.plant_waterer.needs_watering():
                 self.watering_device.water_my_plants()
+                time.sleep(SLEEP_TIME_WHEN_WATERING()
             else: 
                 self.watering_device.dont_water_my_plants()
-            time.sleep(SLEEP_TIME)
+                time.sleep(SLEEP_TIME)
