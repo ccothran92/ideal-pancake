bd2dcd60e1ca0f0d9daf3512942e58658bf0665c
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-09 15:20:22 -0400

    add more stuff

--- ideal-pancake/v2/kasa_outlet.py
+++ ideal-pancake/v2/kasa_outlet.py
@@ -7,7 +7,7 @@ class KasaOutlet(object):
 
     def __init__(self, ipAddr):
         self.outlet = kasa.SmartPlug(ipAddr) #TODO Self discovery?
-        asyncio.run(self.outlet.update())
+        # asyncio.run(self.outlet.update())
 
 
     def water_my_plants(self):
@@ -20,11 +20,11 @@ class KasaOutlet(object):
         asyncio.run(self._turn_off())
 
     async def _turn_on(self):
-        await self.outlet.update()
+        # await self.outlet.update()
         if not self.outlet.is_on:
             await self.outlet.turn_on()
 
     async def _turn_off(self):
-        await self.outlet.update()
+        # await self.outlet.update()
         if self.outlet.is_on:
             await self.outlet.turn_off()
