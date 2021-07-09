0c19aefe61c3c4647683baac23ed6c07addb48fc
Chris Cothran <chris@Chriss-MacBook-Pro.local>
2021-07-09 16:07:44 -0400

    add more stuff

--- ideal-pancake/v2/turn_off_kasa.py
+++ ideal-pancake/v2/turn_off_kasa.py
@@ -1,7 +1,7 @@
 from kasa_outlet import KasaOutlet
 
 kasa1 = KasaOutlet("192.168.1.2")
-kasa.dont_water_my_plants()
+kasa1.dont_water_my_plants()
 
 kasa2 = KasaOutlet("192.168.1.25")
-kasa.dont_water_my_plants()
+kasa2.dont_water_my_plants()
